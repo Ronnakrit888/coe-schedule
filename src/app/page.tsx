d5a6f071b3d920c0095b66a79bfa4f63ccff821b
DontMakeMeDerp <thanasud.r@kkumail.com>
2024-09-18 17:02:14 +0700

    addSubjectInfo(Component,Page)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import { Stack, Container } from "@mui/material";
-import { Table, Navbar } from "@/shared/components";
+import { Table, Navbar } from "@/shared/components";import SubjectInfo from "@/shared/components/SubjectInfo";
 
 export default function Home() {
   return (
@@ -9,6 +9,7 @@ export default function Home() {
       style={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}
     >
       <Table></Table>
+      <SubjectInfo/>
     </div>
   );
 }
